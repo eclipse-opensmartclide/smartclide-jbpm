4cc727800a40056786ccbdc8d6b5e83a2eabb692
Philip Reimer <reimer@atb-bremen.de>
2021-07-08 21:41:33 +0200

    add custom jbpm server image

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # smartclide-jbpm
+
+SmartCLIDE custom jbpm docker image based on
+[jBPM Workbench Showcase Docker image](https://github.com/jboss-dockerfiles/business-central/tree/master/server).
+
+Includes:
+
+* JBoss Wildfly
+* jBPM Workbench
+* KIE Server
+* Keycloak OIDC Wildfly Adapter
---
+++ docker/bin/start_jbpm-wb.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Update database connection setup"
+./update_db_config.sh
+
+echo "Update keycloak configuration"
+./update_config.sh
+
+echo "Running jBPM Server Full on JBoss Wildfly..."
+exec ./standalone.sh -b "$JBOSS_BIND_ADDRESS" "$EXTRA_OPTS" -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true
+exit $?
